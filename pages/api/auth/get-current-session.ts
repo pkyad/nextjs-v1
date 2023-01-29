0a44c2b4490357de5a10400dab9f60a8bdfb81d6
Pradeep Yadav <pradeepyadav@Pradeeps-MacBook-Air.local>
2023-01-29 10:17:00 +0530

    openapi client test integration

--- pages/api/auth/get-current-session.ts
+++ pages/api/auth/get-current-session.ts
@@ -3,6 +3,25 @@
 import { STATUS_CODES } from '@/shared/constants'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import jwt from 'jsonwebtoken'
+import { Test2Api } from '@/shared/proxy/apis'
+
+import { Configuration, ConfigurationParameters } from '@/shared/proxy/runtime'
+export * from '@/shared/proxy/runtime'
+export * from '@/shared/proxy/apis'
+export * from '@/shared/proxy/models'
+
+const configParams: ConfigurationParameters = {
+  basePath: 'http://localhost:8000',
+  middleware: []
+}
+
+const apiConfig = new Configuration(configParams)
+
+const apiClient = {
+  testApi: new Test2Api(apiConfig)
+}
+
+// type ApiClient = typeof apiClient
 
 /**
  * @swagger
@@ -16,7 +35,13 @@ import jwt from 'jsonwebtoken'
  *       401:
  *         description: if the jwt token is not present in the cookie or its invalid
  */
-const handler = (req: NextApiRequest, res: NextApiResponse): void => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const data = await apiClient.testApi.listtestViews({
+    filePath: 'testfilepath'
+  })
+  // eslint-disable-next-line no-console
+  console.log({ data })
+
   try {
     const data = jwt.verify(
       req.cookies.token as string,
