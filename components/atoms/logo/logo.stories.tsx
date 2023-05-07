0d1d6d5798f80943879e4e0bfb8bc1cf6dca7317
Pradeep Yadav <pradeepyadav@Pradeeps-MacBook-Air.local>
2023-05-07 14:27:02 +0530

    chore: dependency versions update

--- components/atoms/logo/logo.stories.tsx
+++ components/atoms/logo/logo.stories.tsx
@@ -2,6 +2,7 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import * as NextImage from 'next/image'
 import React from 'react'
+import { Button } from 'uilib1'
 import Logo from '.'
 
 const OriginalNextImage = NextImage.default
@@ -18,4 +19,18 @@ const config: ComponentMeta<typeof Logo> = {
 
 export default config
 
-export const Primary: ComponentStory<typeof Logo> = () => <Logo></Logo>
+export const Primary: ComponentStory<typeof Logo> = () => {
+	return (
+		<>
+			<Button
+				onClick={() => {
+					// eslint-disable-next-line no-console
+					console.log('clicked')
+				}}
+			>
+				OK
+			</Button>
+			<Logo></Logo>
+		</>
+	)
+}
