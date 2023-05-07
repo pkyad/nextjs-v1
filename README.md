4acc6169b217a8c592dc43c3a8a0aa074a523408
Pradeep Yadav <pradeepyadav@Pradeeps-MacBook-Air.local>
2023-05-07 23:25:27 +0530

    feat: tsx scripting

--- README.md
+++ README.md
@@ -12,12 +12,19 @@ yarn next-swagger-doc-cli next-swagger-doc.json
 
 To generate openAPI spec file we can run
 
-yarn swagger-gen
+`yarn swagger-gen`
+
+If we are not planning to use it as a backend service then why we have to generate swagger file ?
+A: It has 2 use cases. First to have swagger UI for developer to read API documentation. What , how and when
+and second, we are not planning to use the endpoints directly using axios or fetch calls. Instead we will generate a client module for typesafety
+and then use it
+
+## Proxies to real backend services
 
 to genrate/ update openAPI proxy run
-yarn update-openapi-proxy
+`yarn update-openapi-proxy`
 
-You should not use this in your client side code but instead use this client in the API handlers. These APIs need to be protected with headers token and should be used as inter service communication protocol like GRPC.
+You should not use this in your client side code but instead use this proxy in the API handlers. These APIs need to be protected with headers token and should be used as inter service communication protocol like GRPC.
 
 notes :
 
@@ -25,11 +32,17 @@ notes :
 "postbuild": "cpy 'models/**/*.swagger.yaml' public/openapi --flat",
 ```
 
+## JS Docs
+
+Why ? Here we will document the architechture of the project
+
 run `yarn docs` to genetrate a jsDoc based documentation site
 
 This will generate in ROOT/docs folder so you need to preview using a web server / live server in vscode for e.g.
 
-Bonus: This README.md is the landing page for the docs
+Bonus: The project README.md is the landing page for this docs
+
+## Commits to main and Release management
 
 for commiting use correct message format
 e.g. git commit -m "feat: added feature one"
--- jest.config.js
+++ jest.config.js
@@ -1,30 +1,37 @@
 const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './'
+	// Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+	dir: './'
 })
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleNameMapper: {
-    // Handle module aliases (this will be automatically configured for you soon)
-    '^@/components/(.*)$': '<rootDir>/components/$1',
-    '^@/shared/(.*)$': '<rootDir>/shared/$1',
-    '^@/pages/(.*)$': '<rootDir>/pages/$1',
-    '^@/mocks/(.*)$': '<rootDir>/mocks/$1'
-  },
-  testEnvironment: 'jest-environment-jsdom',
-  collectCoverageFrom: ['**/**/*.{ts,tsx}'],
-  coverageThreshold: {
-    global: {
-      branches: 0,
-      functions: 4,
-      lines: 4,
-      statements: 4,
-    },
-  },
+	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+	moduleNameMapper: {
+		// Handle module aliases (this will be automatically configured for you soon)
+		'^@/components/(.*)$': '<rootDir>/components/$1',
+		'^@/shared/(.*)$': '<rootDir>/shared/$1',
+		'^@/pages/(.*)$': '<rootDir>/pages/$1',
+		'^@/mocks/(.*)$': '<rootDir>/mocks/$1'
+	},
+	testEnvironment: 'jest-environment-jsdom',
+	collectCoverageFrom: [
+		'<rootDir>/components/**/*.{ts,tsx,js,jsx}',
+		'<rootDir>/shared/**/*.{ts,tsx,js,jsx}',
+		'<rootDir>/pages/**/*.{ts,tsx,js,jsx}',
+		'<rootDir>/mocks/**/*.{ts,tsx,js,jsx}',
+		'!<rootDir>/**/*.stories.{ts,tsx,js,jsx}'
+	],
+	coveragePathIgnorePatterns: ['<rootDir>/.yalc', '<rootDir>/stories'],
+	coverageThreshold: {
+		global: {
+			branches: 0,
+			functions: 4,
+			lines: 4,
+			statements: 4
+		}
+	}
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
