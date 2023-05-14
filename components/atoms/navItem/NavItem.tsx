e422537c7d238c4e4c385f229ef3f62605065095
Pradeep Yadav <pradeepyadav@Pradeeps-MacBook-Air.local>
2023-05-15 00:03:14 +0530

    chore: formatting change

--- components/atoms/navItem/NavItem.tsx
+++ components/atoms/navItem/NavItem.tsx
@@ -1,29 +1,33 @@
+import { INavItemProps } from '@/shared/types'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import useStyles from './navItem.styles'
-import { INavItemProps } from '@/shared/types'
 import React from 'react'
+import useStyles from './navItem.styles'
 
-const NavItem = ({ link, label, strict = false }: INavItemProps): JSX.Element => {
-  const { route } = useRouter()
-  const classess = useStyles()
+const NavItem = ({
+	link,
+	label,
+	strict = false
+}: INavItemProps): JSX.Element => {
+	const { route } = useRouter()
+	const classess = useStyles()
 
-  let active
-  if (strict) {
-    active = route === link
-  } else {
-    active = route.includes(link)
-  }
+	let active
+	if (strict) {
+		active = route === link
+	} else {
+		active = route.includes(link)
+	}
 
-  return (
-    <>
-      <li className={active ? 'active' : ''}>
-        <Link href={link}>
-          <span className={classess.link}>{label}</span>
-        </Link>
-      </li>
-    </>
-  )
+	return (
+		<>
+			<li className={active ? 'active' : ''}>
+				<Link href={link}>
+					<span className={classess.link}>{label}</span>
+				</Link>
+			</li>
+		</>
+	)
 }
 
 export default NavItem
