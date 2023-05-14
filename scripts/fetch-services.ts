d0657877e14fbdca9ac4efd1ef6d12740e74bb3c
Pradeep Yadav <pradeepyadav@Pradeeps-MacBook-Air.local>
2023-05-14 15:06:46 +0530

    feat: services client script update to massage spec file

--- scripts/fetch-services.ts
+++ scripts/fetch-services.ts
@@ -1,9 +1,18 @@
 /* eslint-disable no-console */
 import select from '@inquirer/select'
 import { spawnSync } from 'child_process'
+import fs from 'fs'
+import fetch from 'node-fetch'
 import services from '../services.config.json'
 
-console.log('running', services.endpoints)
+console.log('Started.....')
+
+interface ServiceSpec {
+	alias: string
+	stage: string
+	authKey: string
+	prod: string
+}
 
 const main = async (): Promise<void> => {
 	const answer = await select({
@@ -24,7 +33,7 @@ const main = async (): Promise<void> => {
 		]
 	})
 	//
-	console.log(answer)
+	console.log(`Fetching for ${answer}`)
 	let servicesToFetch = []
 	if (answer === 'all') {
 		servicesToFetch = services.endpoints
@@ -33,22 +42,53 @@ const main = async (): Promise<void> => {
 			(service) => service.alias === answer
 		)
 	}
+	if (!fs.existsSync('services')) {
+		fs.mkdirSync('services')
+	}
 
-	servicesToFetch.forEach((service) => {
+	;(servicesToFetch as ServiceSpec[]).forEach(async (service) => {
 		console.log(service)
-		const child = spawnSync(
-			'yarn',
-			[
-				'update-openapi-service',
-				`-i ${service.stage} -o services/${service.alias}`
-			],
-			{
-				cwd: '.',
-				encoding: 'utf-8',
-				stdio: 'inherit'
-			}
-		)
-		child.stderr && console.log(child.stderr)
+		const response = await fetch(service.stage)
+		console.log(response)
+		const spec: any = await response.json()
+		console.log(spec)
+		Object.keys(spec.paths).forEach((endpoint) => {
+			try {
+				delete spec.paths[endpoint].get.tags
+			} catch {}
+			try {
+				delete spec.paths[endpoint].post.tags
+			} catch {}
+			try {
+				delete spec.paths[endpoint].delete.tags
+			} catch {}
+			try {
+				delete spec.paths[endpoint].patch.tags
+			} catch {}
+			try {
+				delete spec.paths[endpoint].put.tags
+			} catch {}
+		})
+		const specPath = `services/${service.alias}.json`
+
+		try {
+			fs.writeFileSync(specPath, JSON.stringify(spec))
+			const child = spawnSync(
+				'yarn',
+				[
+					'update-openapi-service',
+					`-i ${specPath} -o services/${service.alias}`
+				],
+				{
+					cwd: '.',
+					encoding: 'utf-8',
+					stdio: 'inherit'
+				}
+			)
+			child.stderr && console.log(child.stderr)
+		} catch (err) {
+			console.error(err)
+		}
 	})
 }
 main()
